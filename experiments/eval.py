2338d041dd1baf8e9e797d820a9ae559950de0a4
Georgsiedel <georgsiedel@gmx.de>
2023-09-05 11:53:39 +0200

    results tinyimagenet

--- experiments/eval.py
+++ experiments/eval.py
@@ -222,10 +222,10 @@ def eval_metric(modelfilename, test_corruptions, combine_test_corruptions, test_
         else:
             print('No corrupted benchmark available other than CIFAR10-c, CIFAR100-c, TinyImageNet-c and ImageNet-c.')
 
+        rmsce_c = np.average(np.asarray(rmsce_c_list))
         print("Average Robust Accuracy (all 19 corruptions): ",sum(accs[2:21])/19,"%, Average Robust Accuracy (15 corruptions): ",sum(accs[2:17])/15,"%, RMSCE-C: ", rmsce_c)
         accs.append(sum(accs[2:21])/19)
         accs.append(sum(accs[2:17])/15)
-        rmsce_c = np.average(np.asarray(rmsce_c_list))
         accs.append(rmsce_c)
 
     if calculate_adv_distance == True:
