a929523083910d057ae407148b5b8816d424b57a
Georgsiedel <georgsiedel@gmx.de>
2023-11-02 14:39:15 +0100

    results

--- results/TinyImageNet/ResNeXt29_32x4d/config115_CosineAnnealingWarmRestarts_separate.py
+++ results/TinyImageNet/ResNeXt29_32x4d/config115_CosineAnnealingWarmRestarts_separate.py
@@ -2,8 +2,8 @@ import numpy as np
 import torchvision.models.mobilenet
 
 train_corruptions = np.array([
-#['standard', 0.0, False],
-['uniform-l0-impulse', 0.02, True]
+['standard', 0.0, False],
+#['uniform-l0-impulse', 0.02, True]
 ])
 
 batchsize = 384
